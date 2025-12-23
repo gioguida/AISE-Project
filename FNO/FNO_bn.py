4a29e3c070237794048f94dc91b94f4fbb436e46
Giovanni Guidarini <gguidarini@ethz.ch>
2025-12-23 12:02:48 +0100

    Problem 2 update

---
+++ FNO/FNO_bn.py
@@ -0,0 +1,143 @@
+import torch
+import torch.nn as nn
+import os
+import numpy as np
+from torch.utils.data import DataLoader, TensorDataset
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+
+class SpectralConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes1):
+        super(SpectralConv1d, self).__init__()
+
+        """
+        1D Fourier layer. It does FFT, linear transform, and Inverse FFT.
+        """
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1
+
+        self.scale = (1 / (in_channels * out_channels))
+        self.weights1 = nn.Parameter(self.scale * torch.rand(in_channels, out_channels, self.modes1, dtype=torch.cfloat))
+
+    # Complex multiplication
+    def compl_mul1d(self, input, weights):
+        # (batch, in_channel, x ), (in_channel, out_channel, x) -> (batch, out_channel, x)
+        return torch.einsum("bix,iox->box", input, weights)
+
+    def forward(self, x):
+        batchsize = x.shape[0]
+        # x.shape == [batch_size, in_channels, number of grid points]
+        
+        # Compute Fourier coefficients
+        x_ft = torch.fft.rfft(x)
+
+        # Multiply relevant Fourier modes
+        out_ft = torch.zeros(batchsize, self.out_channels, x.size(-1) // 2 + 1, device=x.device, dtype=torch.cfloat)
+        out_ft[:, :, :self.modes1] = self.compl_mul1d(x_ft[:, :, :self.modes1], self.weights1)
+
+        # Return to physical space
+        x = torch.fft.irfft(out_ft, n=x.size(-1))
+        return x
+    
+#---------------------
+# Time-conditional BN:
+#---------------------
+class FILM(torch.nn.Module):
+    def __init__(self,
+                channels,
+                use_bn = True):
+        super(FILM, self).__init__()
+        self.channels = channels
+
+        self.inp2scale = nn.Linear(in_features=1, out_features=channels, bias=True)
+        self.inp2bias = nn.Linear(in_features=1, out_features=channels, bias=True)
+
+        self.inp2scale.weight.data.fill_(0)
+        self.inp2scale.bias.data.fill_(0)
+        self.inp2bias.weight.data.fill_(0)
+        self.inp2bias.bias.data.fill_(0)
+
+        if use_bn:
+          self.norm = nn.BatchNorm1d(channels)
+        else:
+          self.norm = nn.Identity()
+
+    def forward(self, x, time):
+
+        x = self.norm(x)
+        time = time.reshape(-1,1).type_as(x)
+        scale     = self.inp2scale(time)
+        bias      = self.inp2bias(time)
+        scale = scale.unsqueeze(2).expand_as(x)
+        bias  = bias.unsqueeze(2).expand_as(x)
+
+        return x * (1. + scale) + bias
+    
+class FNO1d_bn(nn.Module):
+    def __init__(self, modes, width, use_bn = True):
+        super(FNO1d_bn, self).__init__()
+
+        """
+        The overall network. It contains 4 layers of the Fourier layer.
+        1. Lift the input to the desire channel dimension by self.fc0 .
+        2. 4 layers of the integral operators u' = (W + K)(u).
+            W defined by self.w; K defined by self.conv .
+        3. Project from the channel space to the output space by self.fc1 and self.fc2 .
+
+        input: the solution of the initial condition and location (a(x), x)
+        input shape: (batchsize, x=s, c=2)
+        output: the solution of a later timestep
+        output shape: (batchsize, x=s, c=1)
+        """
+
+        self.modes1 = modes
+        self.width = width
+        self.padding = 1  # pad the domain if input is non-periodic
+        self.linear_p = nn.Linear(3, self.width)  # input channel is 2: (u0(x), x) --> GRID IS INCLUDED!
+
+        self.spect1 = SpectralConv1d(self.width, self.width, self.modes1)
+        self.spect2 = SpectralConv1d(self.width, self.width, self.modes1)
+        self.spect3 = SpectralConv1d(self.width, self.width, self.modes1)
+        self.lin0 = nn.Conv1d(self.width, self.width, 1)
+        self.lin1 = nn.Conv1d(self.width, self.width, 1)
+        self.lin2 = nn.Conv1d(self.width, self.width, 1)
+
+        self.linear_q = nn.Linear(self.width, 32)
+        self.output_layer = nn.Linear(32, 1)
+
+        # Batch Normalization
+        self.batch_norm  = FILM(self.width, use_bn)
+        self.activation = torch.nn.Tanh()
+
+    def fourier_layer(self, x, time, spectral_layer, conv_layer):
+        x = spectral_layer(x) + conv_layer(x)
+        x = self.batch_norm(x, time)  
+        return self.activation(x)
+
+    def linear_layer(self, x, linear_transformation):
+        return self.activation(linear_transformation(x))
+
+    def forward(self, x, time):
+        # grid = self.get_grid(x.shape, x.device)
+        # x = torch.cat((x, grid), dim=-1)
+        time_expanded = time.reshape(-1, 1, 1).expand(-1, x.shape[1], 1)  # (batch, spatial_points, 1)
+        # Concatenate: (u0(x), grid, time)
+        x = torch.cat([x, time_expanded], dim=-1)  # (batch, spatial_points, 3)
+
+        x = self.linear_p(x)
+        x = x.permute(0, 2, 1)
+
+        # x = F.pad(x, [0, self.padding])  # pad the domain if input is non-periodic
+
+        x = self.fourier_layer(x, time, self.spect1, self.lin0)
+        x = self.fourier_layer(x, time, self.spect2, self.lin1)
+        x = self.fourier_layer(x, time, self.spect3, self.lin2)
+
+        # x = x[..., :-self.padding]  # pad the domain if input is non-periodic
+        x = x.permute(0, 2, 1)
+
+        x = self.linear_layer(x, self.linear_q)
+        x = self.output_layer(x)
+        return x
